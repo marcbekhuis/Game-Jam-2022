f49d8876b360ed0bd62a33375b38e7956676c3f9
Marc <marc@bekhuis.net>
2022-04-11 13:42:10 +0200

    Player Movement Upgrade

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,47 +10,69 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Jump")]
     [SerializeField] private float jumpForce = 5;
+    [SerializeField] private int jumps = 2;
     [SerializeField] private float jumpCooldown = 1;
     private float jumpTimer = 0;
+    private int jumpCounter;
 
     [Header("Ground Check")]
     [SerializeField] private Vector2 halfSize;
     [SerializeField] private Vector2 offset;
     [SerializeField] private LayerMask checkLayers;
+    [Header("References")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     private Rigidbody2D rigidbody;
+
+    [HideInInspector] public bool onGround = true;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        jumpCounter = jumps;
+        jumpTimer = Time.time;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        GroundCheck();
         int x = System.Convert.ToInt32(Input.GetKey(KeyCode.D)) - System.Convert.ToInt32(Input.GetKey(KeyCode.A));
         float speed = walkSpeed;
         if (Input.GetKey(KeyCode.LeftShift)) speed = runSpeed;
         
         rigidbody.velocity = new Vector2(x * speed, rigidbody.velocity.y);
         Jump();
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        spriteRenderer.flipX = rigidbody.velocity.x < 0;
     }
 
     private void Jump()
     {
+        if (onGround) jumpCounter = jumps;
+
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
         {
-            if (GroundCheck() && Time.time > jumpTimer)
+            if (jumpCounter > 1 && Time.time > jumpTimer)
             {
                 rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
                 jumpTimer = Time.time + jumpCooldown;
+                jumpCounter--;
             }
         }
     }
 
-    private bool GroundCheck()
+    private void GroundCheck()
     {
-        return Physics2D.OverlapBox((Vector2)this.transform.position + offset, halfSize, 0, checkLayers);
+        onGround = Physics2D.OverlapBox((Vector2)this.transform.position + offset, halfSize, 0, checkLayers);
     }
 
     private void OnDrawGizmos()
