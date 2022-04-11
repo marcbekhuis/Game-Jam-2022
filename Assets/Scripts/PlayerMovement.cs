0a57d130584f28454ee68c490fbc29d5cec46fd7
Marc <marc@bekhuis.net>
2022-04-11 13:02:51 +0200

    Improved player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,16 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float walkSpeed = 3;
     [SerializeField] private float runSpeed = 6;
+
+    [Header("Jump")]
     [SerializeField] private float jumpForce = 5;
+    [SerializeField] private float jumpCooldown = 1;
+    private float jumpTimer = 0;
+
+    [Header("Ground Check")]
+    [SerializeField] private Vector2 halfSize;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private LayerMask checkLayers;
 
     private Rigidbody2D rigidbody;
 
@@ -24,6 +33,29 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift)) speed = runSpeed;
         
         rigidbody.velocity = new Vector2(x * speed, rigidbody.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space)) rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        {
+            if (GroundCheck() && Time.time > jumpTimer)
+            {
+                rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                jumpTimer = Time.time + jumpCooldown;
+            }
+        }
+    }
+
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapBox((Vector2)this.transform.position + offset, halfSize, 0, checkLayers);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube((Vector2)this.transform.position + offset, halfSize);
     }
 }
